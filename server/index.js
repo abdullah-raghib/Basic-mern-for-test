46c23a1a69fe08cc0631e918fc99fd00adcc50a4
Abdullah Raghib <abdullah.sid786@gmail.com>
2023-07-09 19:27:17 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const dotenv = require("dotenv");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods:["GET", "POST", "PUT", "DELETE"]
+}));
 const router = require("./router/Auth");
 app.use('/', router)
 
